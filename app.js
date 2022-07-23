aa669936fb542f5fcf9187ad37fd97fc1930d182
thinunes <thiago.tnunes@mercadolivre.com>
2022-07-23 13:38:18 -0300

    att de estudos

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express")
+
+const app = express()
+
+let produtos = require("./produtos")
+
+app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+
+    for(i =0 ; i < produtos.length; i++){
+        produtos[i].id = i+ 1
+    }
+
+res.json(produtos)
+
+})
+
+app.post('/produtos', (req, res)=>{
+
+    let corp = req.body
+
+    produtos = [...produtos, corp]
+
+    res.json(produtos)
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen((3000),()=>{
+    console.log("servidor no ar")
+})
