728b9d54ecee579e697d8be3eec9a87a837709f7
thinunes <thiago.tnunes@mercadolivre.com>
2022-07-25 15:09:36 -0300

    novas rotas

--- app.js
+++ app.js
@@ -2,94 +2,16 @@ const express = require("express")
 
 const app = express()
 
-let produtos = require("./produtos")
+
 
 app.use(express.json())
 
 
-// estou colocando id nos objs e pegando os prudutos.
-app.get('/', (req, res)=>{
-
-    for(i =0 ; i < produtos.length; i++){
-        produtos[i].id = i+ 1
-    }
-
-    res.json(produtos)
-    
-})
-
-//precisa passar pelo app.use(express.json())
-
-//posta um produto que eu passar pelo body
-app.post('/produtos', (req, res)=>{
-
-    let corp = req.body
-
-    produtos = [...produtos, corp]
-
-    res.json(produtos)
-})
+const rotaProdutos = require('./routes/routeProdutos')
 
 
-//pega a um unico produto passando por um parametro
-app.get('/produtos/:id/:details?', (req, res)=>{
 
-    const id = Number(req.params.id)
-
-    const details = req.params.details || "sem descrição"
-
-    const product = produtos.find((produto)=> produto.id === id)
-
-    res.status(200).json(product)
-})
-
-// atualiza um produto mas precisa vir com todas as caracteristicas de obj ja definidos
-app.put('/produtos/:id', (req, res)=>{
-    const id = Number(req.params.id)
-    const content = req.body
-
-    const product = produtos.find((produto)=> produto.id === id)
-
-    if(!product){
-        res.status(400).json({"mensage": "produto noa encontrado"})
-    }
-
-
-   const produtoAtt =  produtos.map((produto)=>{
-        if(produto.id === id){
-            return content
-        }
-        return produto
-    })
-
-    
-    produtos = produtoAtt
-res.status(200).json(produtos)
-
-})
-
-
-//deleta o produto pelo id passar no parametro
-app.delete('/produtos/:id', (req, res)=>{
-    const id = Number(req.params.id)
-
-    const product = produtos.find((produto)=> produto.id === id)
-
-    if(!product){
-        res.status(400).json({"mensage": "produto noa encontrado"})
-    }
-
-    produtos = produtos.filter((produto)=>{
-
-        return produto.id !== id
-
-    })
-
-    res.status(200).json(produtos)
-
-})
-
-
+app.use('/produtos', rotaProdutos)
 
 
 
---
+++ routes/routeProdutos.js
@@ -0,0 +1,90 @@
+const express = require('express')
+
+const routes = express.Router()
+
+let produtos = require("../produtos")
+
+routes.use(express.json())
+
+routes.get('/', (req, res)=>{
+
+    for(i =0 ; i < produtos.length; i++){
+        produtos[i].id = i+ 1
+    }
+
+    res.json(produtos)
+    
+})
+
+//precisa passar pelo app.use(express.json())
+
+//posta um produto que eu passar pelo body
+routes.post('/', (req, res)=>{
+
+    let corp = req.body
+
+   const newProdutos = [...produtos, corp]
+
+    res.json(newProdutos)
+})
+
+
+//pega a um unico produto passando por um parametro
+routes.get('/:id/:details?', (req, res)=>{
+
+    const id = Number(req.params.id)
+
+    const details = req.params.details || "sem descrição"
+
+    const product = produtos.find((produto)=> produto.id === id)
+
+    res.status(200).json(product)
+})
+
+// atualiza um produto mas precisa vir com todas as caracteristicas de obj ja definidos
+routes.put('/:id', (req, res)=>{
+    const id = Number(req.params.id)
+    const content = req.body
+
+    const product = produtos.find((produto)=> produto.id === id)
+
+    if(!product){
+        res.status(400).json({"mensage": "produto noa encontrado"})
+    }
+
+
+   const produtoAtt =  produtos.map((produto)=>{
+        if(produto.id === id){
+            return content
+        }
+        return produto
+    })
+
+    
+    produtos = produtoAtt
+res.status(200).json(produtos)
+
+})
+
+
+//deleta o produto pelo id passar no parametro
+routes.delete('/:id', (req, res)=>{
+    const id = Number(req.params.id)
+
+    const product = produtos.find((produto)=> produto.id === id)
+
+    if(!product){
+        res.status(400).json({"mensage": "produto noa encontrado"})
+    }
+
+    produtos = produtos.filter((produto)=>{
+
+        return produto.id !== id
+
+    })
+
+    res.status(200).json(produtos)
+
+})
+
+module.exports = routes
